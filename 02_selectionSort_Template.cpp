13116d462783348405fd15d379e65710399540c7
Brian Foster <brriann@gmail.com>
2021-02-17 21:30:34 -0700

    Update 02_selectionSort_Template.cpp

--- 02_selectionSort_Template.cpp
+++ 02_selectionSort_Template.cpp
@@ -1,21 +1,6 @@
 // This is code transcribed from 'Absolute C++' (Savitch) for a Foothill College assignment. 
 
 template<class T>
-void sort(T a[], int numberUsed)
-{
-    int indexOfNextSmallest;
-    for (int index = 0; index < numberUsed - 1; index++)
-    {// Place the correct value in a[index]
-        indexOfNextSmallest = 
-            indexOfSmallest(a, index, numberUsed);
-        swapValues(a[index], a[indexOfNextSmallest]);
-        // a[0] <= a[1] <=...<= a[index] are the smallest of the original
-        // array elements.  The rest of the elements are in the remaining
-        // positions.
-    }
-}
-
-template<class T>
 void swapValues(T& variable1, T& variable2)
 {
     T temp;
@@ -40,4 +25,19 @@ int indexOfSmallest(const T a[], int startIndex, int numberUsed)
         }
     }
     return indexOfMin;
+}
+
+template<class T>
+void sort(T a[], int numberUsed)
+{
+    int indexOfNextSmallest;
+    for (int index = 0; index < numberUsed - 1; index++)
+    {// Place the correct value in a[index]
+        indexOfNextSmallest = 
+            indexOfSmallest(a, index, numberUsed);
+        swapValues(a[index], a[indexOfNextSmallest]);
+        // a[0] <= a[1] <=...<= a[index] are the smallest of the original
+        // array elements.  The rest of the elements are in the remaining
+        // positions.
+    }
 }

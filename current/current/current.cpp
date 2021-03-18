152809336b756a43deeda0ba33e630e321bc793d
Brian Foster <brriann@gmail.com>
2021-03-17 21:54:12 -0600

    word puzzle in progress

--- current/current/current.cpp
+++ current/current/current.cpp
@@ -1,64 +1,119 @@
-// Binary search - iterative
-
 #include <iostream>
+#include <string>
+#include <map>
+#include "matrix.cpp"
 
-using std::cin;
+using std::string;
+using std::map;
 using std::cout;
 using std::endl;
 
-const int ARRAY_SIZE = 10;
+string boolToString(bool stringifyMe)
+{
+   string rep = stringifyMe ? "true" : "false";
+   return rep;
+}
 
-void search(const int a[], int first, int last, int key, bool& found, int& location);
+string toString(char* a, int size)
+{
+   string s = "";
+   for (int i = 0; i < size; i++)
+   {
+      s = s + a[i];
+   }
+   return s;
+}
+
+bool searchPuzzle(string word, matrix<char>& puzzle)
+{
+   size_t dim = puzzle.numCols();
+   size_t wordLen = word.length();
+   
+   // iterate rows
+   for (size_t i = 0; i < dim; i++)
+   {
+      // iterate cols
+      for (size_t j = 0; j < dim; j++)
+      {
+         // search forward horizontal
+         if (dim - j < wordLen)
+         {
+            continue;
+         }
+
+         char* peek = new char[wordLen];
+         for (size_t k = 0; k < wordLen; k++)
+         {
+            peek[k] = puzzle[i][j + k];
+         }
+         if (word == toString(peek, wordLen))
+         {
+            delete[] peek;
+            peek = nullptr;
+            return true;
+         }
+         delete[] peek;
+         peek = nullptr;
+
+         // search down vertical
+         if (dim - i < wordLen)
+         {
+            continue;
+         }
+
+         peek = new char[wordLen];
+         for (size_t l = 0; l < wordLen; l++)
+         {
+            peek[l] = puzzle[i+l][j];
+         }
+         if (word == toString(peek, wordLen))
+         {
+            delete[] peek;
+            peek = nullptr;
+            return true;
+         }
+         delete[] peek;
+         peek = nullptr;
+      }
+   }
+}
 
 int main()
 {
-   int a[ARRAY_SIZE] = { 1, 1, 3, 5, 6, 7, 8, 9, 11, 12 };
-   const int finalIndex = ARRAY_SIZE - 1;
+   vector<vector<char> > wordPuzzleVec = 
+   { 
+      {'c', 'o', 'l', 'a', 'x'},
+      {'x', 'r', 'i', 'x', 'x'},
+      {'x', 'a', 't', 'x', 'x'},
+      {'x', 'w', 'x', 'a', 'x'},
+      {'t', 'a', 'c', 'x', 'r'},
+   };
 
-   int key, location;
-   bool found;
-
-   cout << "Enter number to be located: ";
-   cin >> key;
-
-   search(a, 0, finalIndex, key, found, location);
-
-   if (found)
+   vector<string> wordList = 
    {
-      cout << key << " is in index location " << location << endl;
+      "cola",
+      "cat",
+      "rat",
+      "war",
+      "lit",
+   };
+
+   map<string, bool> results;
+
+   matrix<char> wordPuzzle = matrix<char>(wordPuzzleVec);
+
+   // search for each word
+   for (string word : wordList)
+   {
+      results[word] = searchPuzzle(word, wordPuzzle);
    }
-   else
+
+   // display results
+   map<string, bool>::iterator it;
+   for (it = results.begin(); it != results.end(); it++)
    {
-      cout << key << " is not in the array." << endl;
+      cout << it->first << " = " << boolToString(it->second) << endl;
    }
 
    return 0;
-}
-
-void search(const int a[], int first, int last, int key, bool& found, int& location)
-{
-   int firstCopy = first;
-   int lastCopy = last;
-   int mid;
-
-   found = false; //initialize
-
-   while ((first <= last) && !(found))
-   {
-      mid = ((first + last) / 2);
-
-      if (key == a[mid])
-      {
-         found = true;
-         location = mid;
-      }
-      else if (key < a[mid])
-      {
-         last = mid - 1;
-      }
-      else if (key > a[mid])
-      {
-         first = mid + 1;
-      }
-   }
 }

ef7a94594d00e7862f8db4a52dac4432d6b394ab
Brian Foster <brriann@gmail.com>
2021-04-28 22:50:18 -0600

    postfix expression evaluator and converter

--- cs2c/postfixExpression.h
+++ cs2c/postfixExpression.h
@@ -3,17 +3,24 @@
 #define postfix_expression_h
 
 #include <string>
+#include <sstream>
 #include <set>
+#include <map>
 #include <stack>
 
 using std::string;
+using std::stringstream;
 using std::set;
+using std::pair;
+using std::make_pair;
+using std::map;
 using std::stack;
 
 namespace postfixExpression {
    // valid operators
    set<char> operators{ '*', '+' };
 
+   // abc*+de*f+g*+
    int evaluatePostfixExpression(string postfixExpression) {
       stack<int> parsedSymbols;
       // default value
@@ -50,6 +57,72 @@ namespace postfixExpression {
       }
 
       return result;
+   }
+
+   // a+b*c+(d*e+f)*g
+   // abc*+de*f+g*+
+   string convertInfixToPostfix(string postfixExpression) {
+
+      const char LEFT_PAREN = '(';
+      const char RIGHT_PAREN = ')';
+
+      // valid operators
+      set<char> operators{ '*', '+', '(', ')' };
+
+      map<char, int> inputPrecedence;
+      inputPrecedence['+'] = 2;
+      inputPrecedence['*'] = 4;
+      inputPrecedence['('] = 5;
+
+      map<char, int> stackPrecedence;
+      inputPrecedence['('] = 1;
+      inputPrecedence['+'] = 2;
+      inputPrecedence['*'] = 4;
+
+      stack<char> parsedOperators;
+
+      stringstream output;
+
+      for (char c : postfixExpression) {
+         // if symbol is a number (aka, "not an operator")
+         if (operators.find(c) == operators.end())
+         {
+            output << c;
+         }
+         else { // c is operator
+            switch (c) {
+               case RIGHT_PAREN:
+                  while (!parsedOperators.empty() && parsedOperators.top() != LEFT_PAREN) {
+                     // output operators until left paren is reached
+                     output << parsedOperators.top();
+                     parsedOperators.pop();
+                  }
+                  // pop left paren, but don't output
+                  if (!parsedOperators.empty()) {
+                     parsedOperators.pop();
+                  }
+                  break;
+               default:
+                  if (parsedOperators.empty()) {
+                     parsedOperators.push(c);
+                  }
+                  else {
+                     while (!parsedOperators.empty() && inputPrecedence[c] <= stackPrecedence[parsedOperators.top()]) {
+                        output << parsedOperators.top();
+                        parsedOperators.pop();
+                        if (parsedOperators.empty()) {
+                           break;
+                        }
+                     }
+                     parsedOperators.push(c);
+                  }
+                  
+                  break;
+            }
+         }
+      }
+
+      return output.str();
    }
 }
 
--- current/current/current.cpp
+++ current/current/current.cpp
@@ -5,6 +5,7 @@
 #include "current.h"
 
 using std::string;
+using postfixExpression::convertInfixToPostfix;
 using postfixExpression::evaluatePostfixExpression;
 
 // a + b * c + ( d * e + f ) * g
@@ -13,10 +14,29 @@ using postfixExpression::evaluatePostfixExpression;
 // 6* (5 + ((2 + 3) * 8)) + 3)
 
 const string POSTFIX_EXPRESSION = "6523+8*+3+*";
+const string INFIX_EXPRESSION = "6*(5+((2+3)*8))+3)";
+
+map<string, string> expressionMap = {
+   {"((((3+5)*1)*(2*(8+9)))*2)", "35+1*289+**2*"},
+   {"6*(5+((2+3)*8))+3)", "6523+8*+3+*"},
+   {"(((1+2)))", "12+"},
+};
 
 int main()
 {
-   std::cout << "result: " << evaluatePostfixExpression(POSTFIX_EXPRESSION) << std::endl;
+   for (auto str : {
+      "6523+8*+3+*",
+      "65+238*3++*",
+      "12+"
+      }) {
+      std::cout << str << " : " << evaluatePostfixExpression(str) << std::endl;
+   }
+
+   for (const auto &expressionPair : expressionMap) {
+      std::cout << expressionPair.first << " : " << expressionPair.second << " : " << convertInfixToPostfix(expressionPair.first) << " : " << evaluatePostfixExpression(convertInfixToPostfix(expressionPair.first)) << std::endl;
+   }
+
+   //std::cout << INFIX_EXPRESSION << " : " << convertInfixToPostfix(INFIX_EXPRESSION) << std::endl;
 
    return 0;
 }

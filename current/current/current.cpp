1f51254dc0ae182853baf2a2e71d21878ac27754
Brian Foster <brriann@gmail.com>
2021-05-01 13:09:12 -0600

    update current

--- current/current/current.cpp
+++ current/current/current.cpp
@@ -1,42 +1,43 @@
-
 #include <iostream>
+#include <set>
 #include <string>
+#include <cctype>
 
-#include "current.h"
+using namespace std;
 
-using std::string;
-using postfixExpression::convertInfixToPostfix;
-using postfixExpression::evaluatePostfixExpression;
-
-// a + b * c + ( d * e + f ) * g
-// a b c * + d e * f + g * +
-
-// 6* (5 + ((2 + 3) * 8)) + 3)
-
-const string POSTFIX_EXPRESSION = "6523+8*+3+*";
-const string INFIX_EXPRESSION = "6*(5+((2+3)*8))+3)";
-
-map<string, string> expressionMap = {
-   {"((((3+5)*1)*(2*(8+9)))*2)", "35+1*289+**2*"},
-   {"6*(5+((2+3)*8))+3)", "6523+8*+3+*"},
-   {"(((1+2)))", "12+"},
-};
-
-int main()
-{
-   for (auto str : {
-      "6523+8*+3+*",
-      "65+238*3++*",
-      "12+"
-      }) {
-      std::cout << str << " : " << evaluatePostfixExpression(str) << std::endl;
+// requires #include <cctype> for std::isupper
+void removeCapitals(std::set<std::string>* stringSet) {
+   auto it = stringSet->begin();
+   while (it != stringSet->end()) {
+      bool hasUpper = false;
+      for (auto c : *it) {
+         if (std::isupper(c)) {
+            hasUpper = true;
+            break;
+         }
+      }
+      if (hasUpper) {
+         it = stringSet->erase(it);
+      }
+      else { // no erase happened, need to increment iterator
+         ++it;
+      }
    }
+}
 
-   for (const auto &expressionPair : expressionMap) {
-      std::cout << expressionPair.first << " : " << expressionPair.second << " : " << convertInfixToPostfix(expressionPair.first) << " : " << evaluatePostfixExpression(convertInfixToPostfix(expressionPair.first)) << std::endl;
+
+int main() {
+   set<string> stringSet = { "this", "THESE", "word", "wordS", "group", "gO", "stays" };
+
+   for (auto s : stringSet) {
+      cout << s << endl;
    }
+   cout << endl << "-------------------" << endl;
+   removeCapitals(&stringSet);
 
-   //std::cout << INFIX_EXPRESSION << " : " << convertInfixToPostfix(INFIX_EXPRESSION) << std::endl;
+   for (auto s : stringSet) {
+      cout << s << endl;
+   }
 
    return 0;
 }
